52262d01572bccf9a8ad2fc492a0f863107448ea
Araav Bhardwaj <146269306+araav-bhardwaj@users.noreply.github.com>
2023-10-10 15:12:42 +0000

    commit

--- file2.c
+++ file2.c
@@ -1,1 +1,11 @@
 #include <stdio.h>
+int main(){
+    int var;
+    var = 10;
+    int input;
+    printf("Enter the input : \n");
+    scanf("%d" , &input);
+    printf("here is the output with a 10 in the start will be:");
+    printf("%d \n %d \n ",var ,input);
+    return 0;
+}
